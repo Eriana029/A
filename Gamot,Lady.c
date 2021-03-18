686a255d6307536beecf9cd5b2e94a76d3b88bff
LADY <80914127+Eriana029@users.noreply.github.com>
2021-03-19 02:02:07 +0800

    Add files via upload

---
+++ Gamot,Lady.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+void main()
+{
+  int vote_age= 17;
+  printf("Name: Juan Delama De Guzman \n");
+
+  printf("Input the age of the candidate : ");
+  scanf("%d",&vote_age);
+  if (vote_age<18)
+     {
+       printf("Invalid Age, Please Try Again.");
+     }
+  else
+     printf("Congratulation! You are eligible for casting your vote.\n");
+}
+
