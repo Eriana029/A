a0e49551a93ae0d37717c55b6259d7fa0a650b55
LADY <80914127+Eriana029@users.noreply.github.com>
2021-03-19 01:28:35 +0800

    Add files via upload

---
+++ Gamot,L.c
@@ -0,0 +1,19 @@
+#include <stdio.h>
+int main()
+// Output 2
+{
+    int a=15 , b=5, sum, diff, product, c;
+
+    sum= a+b;
+    printf("a+b= %d\n",sum);
+
+    diff= a-b;
+    printf("a-b= %d\n",diff);
+
+
+    product= a*b;
+    printf("a*b= %d\n",product);
+
+    c= a/b;
+    printf("a/b= %d\n",c);
+}
---
+++ Gamot,La.c
@@ -0,0 +1,19 @@
+#include <stdio.h>
+int main()
+// Output 1
+{
+    int a=16 , b=8, sum, diff, product, c;
+
+    sum= a+b;
+    printf("a+b= %d\n",sum);
+
+    diff= a-b;
+    printf("a-b= %d\n",diff);
+
+
+    product= a*b;
+    printf("a*b= %d\n",product);
+
+    c= a/b;
+    printf("a/b= %d\n",c);
+}
