a9ff30378a442ab38a3e5488fb8e1b2171bc7a8a
LADY <80914127+Eriana029@users.noreply.github.com>
2021-04-13 11:11:50 +0800

    Add files via upload

---
+++ ID  PIN.c
@@ -0,0 +1,25 @@
+#include <stdio.h>
+
+int main()
+{
+    int Id_Number, Pin;
+
+
+    printf("Enter your Id Number:\n");
+    scanf("%d",&Id_Number);
+    if(Id_Number == 2021);
+
+    printf("Enter your Pin:\n");
+    scanf("%d",&Pin);
+
+    if(Pin == 2000)
+{
+    printf("\n You have Successfully Logged In!");
+    printf ("\n Id_Number: 2021");
+}
+    else
+{
+    printf("\n Invalid ID/pin");
+}
+    return 0;
+}
